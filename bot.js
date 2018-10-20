1fb4d7ed14eb31d45d11f0a688a140d0f597aca2
OmarEssamer <43882047+OmarEssamer@users.noreply.github.com>
2018-10-20 16:59:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setActivity('', { type: 'WATCHING' })
+client.user.setActivity('Dream System', { type: 'WATCHING' })
 });
 
 client.on('message', msg => {
